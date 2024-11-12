99b040d728697c4f44ffe0feffb5373059471555
EdielOliveira <ediel.inacio@outlook.com>
2024-11-12 17:48:03 -0300

    alterado login e senha

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ function logar() {
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if (login == "ediel.silva5@etec.com" && senha == "edieletec") {
+    if (login == "Usuario" && senha == "12345") {
         sessionStorage.setItem('autenticado', 'true');
         location.href = "login.html";
     } else {
